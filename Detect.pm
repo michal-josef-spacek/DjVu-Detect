b2537580423e39a3e7d1c3bbe451679b2e9cf5c0
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2021-11-13 12:35:36 +0100

    Prepare for 0.04 version

--- Detect.pm
+++ Detect.pm
@@ -9,7 +9,7 @@ use File::Which qw(which);
 
 Readonly::Array our @EXPORT_OK => qw(detect_djvu_chunk detect_djvu_file);
 
-our $VERSION = 0.03;
+our $VERSION = 0.04;
 
 sub detect_djvu_chunk {
 	my ($file, $chunk_name) = @_;
@@ -199,6 +199,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.03
+0.04
 
 =cut
